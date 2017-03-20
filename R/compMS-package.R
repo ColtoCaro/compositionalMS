cb4709b4f4e6063ed6e10749f1dfcd7580f29899
Jonathon O'Brien <obrienj@colorado.edu>
2017-03-20 16:01:41 -0400

    Working on Roxygen

---
+++ R/compMS-package.R
@@ -0,0 +1,34 @@
+# File for creating compMS documentation with roxygen
+
+#' Bayesian Compositional Proteomics Modeling via Stan
+#'
+#' @docType package
+#' @name compMS-package
+#' @aliases compMS
+#' @useDynLib rstanarm, .registration = TRUE
+#'
+#'  @import methods
+#'  @importFrom rstan optimizing sampling vb constrain_pars extract
+#'    extract_sparse_parts get_posterior_mean stanc
+#'  @import stats
+#'  @import Rcpp
+#'  @import bayesplot
+#'  @import rstantools
+#'
+#'  @description
+#'  The \pkg{compMS} package enables users to fit compostional proteomics
+#'  models for isobaric tag mass spectrometry experiments.  The package is
+#'  designed to make these models accesible to people without a background
+#'  in Bayesian statistics or compositional data analysis.  This is done by
+#'  requiring users to follow a very specific format for data entry.  Once
+#'  the data if formatted properly everything else is handled internally.
+#'
+#'  @section Model Details:
+#'  This is where I will explain the model and the options for use:
+#'  \describe{
+#'  Here is a detailed description.
+#'  }
+#'
+#'  @template reference-stan-manual
+#'
+NULL
--- R/testR.R
+++ R/testR.R
@@ -1,14 +1,13 @@
 #File to test out package functionality
 
-#' @name test_function
-#' @param text
+#' Testing this stuff out
+#'
+#' This code is just a way for me to learn roxygen.
+#'
 #' @export
 #' @return Some random text
 test_function <- function(text){
   print(paste("You printed:", text))
 }
 
-#Can I get at the Stan function?
-#' @name callStan
-#' @export
-cppcode <- rstan::stanc(file = "~/exec/testModel.stan")
+
