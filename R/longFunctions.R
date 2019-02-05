a175888cf16f5f5c3ab8c7a70291e2cc75702421
Jonathon O'Brien <obrienj@calicolabs.com>
2019-02-04 17:45:07 -0800

    Longitudinal generalized and drop = FALSE to contrast function

---
+++ R/longFunctions.R
@@ -0,0 +1,119 @@
+#Functions to help with longitudinal analysis
+
+
+testInteract <- function(tempDat, timeDegree = 2, fullTimes, useW = TRUE,
+                  refCat = NULL, groupByGene = FALSE){
+
+  if(groupByGene){
+    uProt <- unique(tempDat$Gene)
+    #Model always uses Protein column
+    #Replace column and save the protein names for later
+    savedProts <- tempDat$Protein
+    tempDat$Protein <- tempDat$Gene
+  }else{
+    uProt <- unique(tempDat$Protein)
+    savedProts <- tempDat$Protein
+  }
+
+  nProt <- length(uProt)
+  times <- unique(tempDat$Time)
+
+  degVec <- as.character(1:timeDegree)
+  degVec[1] <- ""
+
+  #fmla <- as.formula(paste("FC ~ 0 + Gene + Gene:Species + Gene:Time + Gene:Time2 +
+   #                        Gene:Species:Time + Gene:Species:Time2 "))
+
+  fmla <- as.formula(paste("FC ~ 0 + Protein + Protein:Category + ",
+                           paste0("Protein:", paste0("Time", degVec), collapse = " + "),
+                            "+", paste0("Protein:Category:", paste0("Time", degVec), collapse = " + ")))
+
+  if(!is.null(refCat)){
+    tempDat$Category <- factor(tempDat$Category)
+    tempDat$Category <- relevel(tempDat$Category, ref = refCat)
+  }else{
+    tempDat$Category <- factor(tempDat$Category)
+  }
+
+  uCats <- levels(tempDat$Category)
+
+  #Create strings that define the model
+  if(useW){
+    fullMod <- lm(fmla, weights = w_, data = tempDat)
+  }else{
+    fullMod <- lm(fmla, data = tempDat)
+  }
+
+
+  #Now get predictions and p-values for each protein
+  pRes <- list()
+
+  for(index in 1:nProt){
+    #Implement F test for time effect
+    #Create strings that define the hypothesis tests
+
+
+
+    timeTests <- list()
+    catTests <- list()
+
+    #Test for an overall time effect in the baseline condition
+    timeStr <-  paste0("Protein", uProt[index], paste0(":Time", degVec), " = 0")
+    timeTests[[1]] <-  lht(fullMod, timeStr)$`Pr(>F)`[2]
+
+
+    for(t_ in 2:length(uCats)){
+      #Test for any difference between refCat and category t_
+      catStr <-  paste0("Protein", uProt[index], ":Category", uCats[t_], c("", paste0(":Time", degVec)), " = 0")
+      catTests[[t_ - 1]] <- lht(fullMod, catStr)$`Pr(>F)`[2]
+
+      #Test for an overall time effect in condition t_
+      timeStr <- paste0("Protein", uProt[index], paste0(":Time", degVec), " + ",
+                        "Protein", uProt[index], ":Category", uCats[t_], paste0(":Time", degVec)," = 0")
+      timeTests[[t_]] <- lht(fullMod, timeStr)$`Pr(>F)`[2]
+    }
+
+    #Now exctract and store the predicted values
+    newDfs <- list()
+    if(timeDegree == 1){
+      newDfs <- lapply(uCats, function(x) data.frame(Protein = uProt[index], Time = times, Category = x))
+    }
+    if(timeDegree == 2){
+      newDfs <- lapply(uCats, function(x) data.frame(Protein = uProt[index], Time = times, Time2 = times^2, Category = x))
+    }
+    if(timeDegree == 3){
+      newDfs <- lapply(uCats, function(x) data.frame(Protein = uProt[index], Time = times, Time2 = times^2, Time3 = times^3, Category = x))
+    }
+
+    catPreds <- lapply(newDfs, function(x) predict(fullMod, x))
+
+    catRes <- list()
+    catRes[[1]] <- data.frame(Gene = tempDat$Gene[match(uProt[index], tempDat$Protein)],
+                              Protein = savedProts[match(uProt[index], tempDat$Protein)],
+                              matrix(catPreds[[1]][match(fullTimes, times)], nrow = 1), timeTests[[1]])
+    colnames(catRes[[1]]) <- c("Gene", "Protein", paste(uCats[1], paste0("Time", fullTimes)), paste(uCats[1], "Pval-Time"))
+
+    for(t_ in 2:length(uCats)){
+      catRes[[t_]] <- data.frame(matrix(catPreds[[t_]][match(fullTimes, times)], nrow = 1), timeTests[[t_]], catTests[[t_ - 1]])
+      colnames(catRes[[t_]]) <- c(paste(uCats[t_], paste0("Time", fullTimes)), paste(uCats[t_], "Pval-Time"), paste0("Pval-", uCats[t_]))
+    }
+
+    pRes[[index]] <- do.call(cbind, catRes)
+
+  }#end Gene loop
+
+  resDf <- do.call(rbind, pRes)
+
+  #Now add columns with q values
+  #first find the p-values
+
+  pIndex <- grep("Pval", colnames(resDf))
+  Qvals <- as.data.frame(lapply(resDf[ , pIndex], function(x) p.adjust(x, method = "fdr")))
+  colnames(Qvals) <- gsub("Pval", "Qval", colnames(resDf)[pIndex])
+  finalDf <- data.frame(resDf, Qvals)
+
+  newIndex <- c(seq_along(resDf), pIndex + 0.5)
+  finalDf <- finalDf[ ,order(newIndex)]
+
+  finalDf
+}
